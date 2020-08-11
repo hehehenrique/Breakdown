e9906d0db2b6b67cc87dac70c5bb3b72cd64b246
hehehenrique <jhenriquemteixeira@gmail.com>
2020-08-11 14:46:35 +0100

    Removed useless function on VehicleController

--- Source/Breakdown/VehicleController.h
+++ Source/Breakdown/VehicleController.h
@@ -14,15 +14,5 @@ UCLASS()
 class BREAKDOWN_API AVehicleController : public APlayerController
 {
 	GENERATED_BODY()
-
 public:
-	void TestFunction();
-
-
-
-	
-	//// Get a texture of a valid friends avatar, STEAM ONLY, Returns invalid texture if the subsystem hasn't loaded that size of avatar yet
-	//UFUNCTION( BlueprintCallable, Category = "Online|AdvancedFriends|SteamAPI", meta = ( ExpandEnumAsExecs = "Result" ) )
-	//	static UTexture2D * GetSteamFriendAvatar( const FBPUniqueNetId UniqueNetId, EBlueprintAsyncResultSwitch &Result, SteamAvatarSize AvatarSize = SteamAvatarSize::SteamAvatar_Medium );
-
 };
